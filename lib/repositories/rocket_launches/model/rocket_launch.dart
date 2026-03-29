a597d9a0a2ad7f08bb9acc6b9cfb04e177adbe48
Vlad <hellomyvolodia@outlook.com>
2026-03-29 19:57:50 +0300

    refactor RocketLaunch model
    (DateTime, nullable fields, extensions)

--- lib/repositories/rocket_launches/model/rocket_launch.dart
+++ lib/repositories/rocket_launches/model/rocket_launch.dart
@@ -1,8 +1,10 @@
+import 'package:intl/intl.dart';
+
 class RocketLaunch {
   final String rocketId;
   final String missionName;
   final String description;
-  final String launchDate;
+  final DateTime launchDate;
   final String websiteLink;
 
   RocketLaunch({
@@ -18,8 +20,14 @@ class RocketLaunch {
       rocketId: json['rocket']['rocket_id'] as String,
       missionName: json['mission_name'] as String,
       description: json['launch_site']['site_name_long'] as String,
-      launchDate: json['launch_date_utc'] as String,
-      websiteLink: json['links']['wikipedia'] as String,
+      launchDate: DateTime.parse(json['launch_date_utc']),
+      websiteLink: json['links']['wikipedia'] ?? '',
     );
   }
+}
+
+extension DateFormatting on DateTime {
+  String get formattedDate => DateFormat('dd/MM/yyyy').format(this);
+
+  String get formattedTime => DateFormat('hh:mm a').format(this);
 }

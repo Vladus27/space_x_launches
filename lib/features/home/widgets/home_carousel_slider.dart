c0df6e8ebb0b792825a2e7325296894a07ff14c3
Vlad <hellomyvolodia@outlook.com>
2026-03-29 19:54:02 +0300

    connect slider index state with carousel and dots
    also connect with rocket list via AsyncValue

--- lib/features/home/widgets/home_carousel_slider.dart
+++ lib/features/home/widgets/home_carousel_slider.dart
@@ -1,20 +1,31 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeCarouselSlider extends StatelessWidget {
-  const HomeCarouselSlider({super.key});
+import 'package:space_x_launches/providers/slider_index_provider.dart';
+import 'package:space_x_launches/repositories/space_rocket/model/rocket.dart';
+
+class HomeCarouselSlider extends ConsumerWidget {
+  const HomeCarouselSlider({super.key, required this.rocketsList});
+  final List<Rocket> rocketsList;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sliderIndexNotifier = ref.read(sliderIndexProvider.notifier);
+
     return CarouselSlider.builder(
-      itemCount: 5,
+      itemCount: rocketsList.length,
       itemBuilder: (context, index, currentIndex) {
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 2),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(10),
+          child: ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(10),
+            child: SizedBox(
+              width: double.infinity,
+              child: Image.network(
+                rocketsList[index].imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         );
@@ -25,6 +36,9 @@ class HomeCarouselSlider extends StatelessWidget {
         height: 194,
         viewportFraction: 0.78,
         enlargeFactor: 0.18,
+        onPageChanged: (index, reason) {
+          sliderIndexNotifier.setIndex(index);
+        },
       ),
     );
   }
--- lib/features/home/widgets/home_indicator_dots.dart
+++ lib/features/home/widgets/home_indicator_dots.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeIndicatorDots extends StatelessWidget {
-  const HomeIndicatorDots({super.key});
+import 'package:space_x_launches/providers/slider_index_provider.dart';
+import 'package:space_x_launches/repositories/space_rocket/model/rocket.dart';
+
+class HomeIndicatorDots extends ConsumerWidget {
+  const HomeIndicatorDots({super.key, required this.rocketsList});
+  final List<Rocket> rocketsList;
 
   @override
-  Widget build(BuildContext context) {
-    var isActive = false;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sliderState = ref.watch(sliderIndexProvider);
+    bool isActive;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(4, (index) {
-        isActive = index == 2 ? true : false;
+      children: List.generate(rocketsList.length, (index) {
+        isActive = sliderState == index ? true : false;
         return Container(
           margin: const EdgeInsets.symmetric(horizontal: 2),
           width: 10,
